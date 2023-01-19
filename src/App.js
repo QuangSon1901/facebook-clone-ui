c56a9a29a345dc83ea30921325016adbcd96e012
FSHuyNguyen <nguyenvanhuy.hs1999@gmail.com>
2023-01-19 11:25:44 +0700

    Header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { MainLayout } from './Layout';
+
 function App() {
-    return <div className="App">Hello World!!!!</div>;
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+                        let Layout = MainLayout;
+
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/assets/images/index.js
@@ -0,0 +1,6 @@
+const images = {
+    facebook_logo: require('~/assets/images/logo.svg').default,
+    account_avatar: require('~/assets/images/account_avatar.jpg'),
+};
+
+export default images;
