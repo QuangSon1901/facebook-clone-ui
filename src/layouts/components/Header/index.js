fa9504b038104d7ef26e8120ee6fd41652ee200c
QuangSon1901 <quangsonbmt760@gmail.com>
2023-02-03 13:15:50 +0700

    responsive header

--- src/layouts/components/Header/index.js
+++ src/layouts/components/Header/index.js
@@ -1,7 +1,9 @@
 import { Link } from 'react-router-dom';
 
-import images from '~/assets/images';
+import Tippy from '@tippyjs/react/headless';
 
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import images from '~/assets/images';
 import config from '~/config';
 import Image from '~/components/Image';
 import Search from '~/layouts/components/Search';
@@ -12,15 +14,58 @@ const Header = () => {
         <header className="header">
             <div className="header__inner">
                 <Link to={config.routes.home} className="header__inner__logo">
-                    <Image src={images.facebookLogin} alt="Logo Facebook" />
+                    <Image src={images.facebookLogin} alt="Logo Facebook" className="header__inner__logo__pc" />
+                    <Image src={images.logoFBMobile} alt="Logo Facebook" className="header__inner__logo__mb" />
+                    <ButtonCircle icon={images.searchIcon} className="header__inner__logo__mb" />
                 </Link>
 
                 <Search />
 
                 <div className="header__inner__actions">
-                    <ButtonCircle icon={images.messageIcon} tippyContent="Messenger" />
-                    <ButtonCircle icon={images.notificationIcon} tippyContent="Thông báo" />
-                    <ButtonCircle img={images.accountAvatar} tippyContent="Tài khoản" />
+                    <div>
+                        <Tippy
+                            interactive
+                            visible={true}
+                            render={(attrs) => (
+                                <div className="header__inner__actions__messsage-wrapper">
+                                    <PopperWrapper>
+                                        <div className="header__inner__actions__messsage-wrapper__main">
+                                            <div className="header__inner__actions__messsage-wrapper__header">
+                                                <div>
+                                                    <h1 className="header__inner__actions__messsage-wrapper__header__title">
+                                                        Chat
+                                                    </h1>
+                                                    <div className="header__inner__actions__messsage-wrapper__header__actions"></div>
+                                                </div>
+                                                <div></div>
+                                            </div>
+                                        </div>
+                                        <div className="header__inner__actions__messsage-wrapper__footer">
+                                            <Link to={config.routes.home}>Xem tất cả trong Messenger</Link>
+                                        </div>
+                                    </PopperWrapper>
+                                </div>
+                            )}
+                        >
+                            <div>
+                                <ButtonCircle
+                                    icon={images.messageIcon}
+                                    hover
+                                    notifyBadge="3"
+                                    tippyContent="Messenger"
+                                    style={{ backgroundColor: 'var(--bg-second)' }}
+                                />
+                            </div>
+                        </Tippy>
+                    </div>
+                    <ButtonCircle
+                        icon={images.notificationIcon}
+                        hover
+                        notifyBadge="10"
+                        tippyContent="Thông báo"
+                        style={{ backgroundColor: 'var(--bg-second)' }}
+                    />
+                    <ButtonCircle img={images.accountAvatar} hover tippyContent="Tài khoản" />
                 </div>
             </div>
         </header>
